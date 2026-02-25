bd7e8b0b4957b353bdc21c5e3acf7184887395f7
Vanshika <vanshikakashyap769@gmail.com>
2026-02-25 14:39:54 +0530

    fix small issue in index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ import reviewRoute from './route/reviewRoute.js';
 import notificationRoute from './route/notificationRoute.js';
 dotenv.config()
 
-const port = process.en.PORT || 5000;
+const port = process.env.PORT || 5000;
 const client = process.env.FRONTEND_URL;
 
 const app = express();
